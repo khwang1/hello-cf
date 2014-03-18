0351e8ee425eba273f8cc981fb46de5482462d13
Cornelia Davis <cornelia@corneliadavis.com>
2014-03-17 17:24:06 -0700

    add second way to crash the app

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ module App
 
     configure do
       enable :logging
+      set :raise_errors, true
     end
 
     before do
@@ -16,12 +17,18 @@ module App
       logger.debug "debug message"
       logger.info "info message"
       port = ENV['VCAP_APP_PORT']
-      "\n<h1>Hello DevBeat 2013 from port #{port}!!</h1>\n"
+      rurl = request.url
+      "\n<h1>Hello World 2014 from port #{port}!! Request url #{rurl}</h1>\n"
     end
 
     get "/broken" do
       logger.debug "trying broken service"
       `ps -AF | grep -m1 "ruby" | awk '{print $2;}' | (read pid; kill $pid)`
+    end
+    
+    get "/systemcrash" do
+      logger.debug "trying broken service"
+      Kernel.exit!
     end
     run!
   end
