4f12a9cbd122115ceec9f5d4738bd3458956f513
Karen Wang <khwang@pivotal.io>
2015-09-29 10:40:46 -0700

    add more logging

--- app.rb
+++ app.rb
@@ -4,28 +4,28 @@ require 'json'
 require 'pp'
 
 module App
-  class Main < Sinatra::Base
+    class Main < Sinatra::Base
 
-    configure do
-      enable :logging
-      set :raise_errors, true
-    end
+        configure do
+            enable :logging
+            set :raise_errors, true
+        end
 
-    before do
-      logger.datetime_format = "%Y/%m/%d @ %H:%M:%S "
-      logger.level = Logger::INFO
-    end
+        before do
+            logger.datetime_format = "%Y/%m/%d @ %H:%M:%S "
+            logger.level = Logger::INFO
+        end
 
-    get '/' do
-      logger.debug "debug message"
-      logger.info "info message"
-      port = ENV['VCAP_APP_PORT']
-      vcap_application=JSON.parse(ENV['VCAP_APPLICATION'] || '{}')
-      instance_number = vcap_application['instance_index']
-      dea_host = vcap_application['host']
+        get '/' do
+            logger.debug "debug message"
+            logger.info "info message"
+            port = ENV['VCAP_APP_PORT']
+            vcap_application=JSON.parse(ENV['VCAP_APPLICATION'] || '{}')
+            instance_number = vcap_application['instance_index']
+            dea_host = vcap_application['host']
 
 
-      sendgrid = Hash.new
+            sendgrid = Hash.new
 =begin
       puts ENV['VCAP_SERVICES']
       if (!ENV['VCAP_SERVICES'].nil? && !ENV['VCAP_SERVICES'].blank?)
@@ -40,34 +40,39 @@ module App
           end
       end
 =end
-    
-      vcap_services=JSON.parse(ENV['VCAP_SERVICES'] || '{}')
-      logger.info pp vcap_services
-      if !vcap_services.empty?
-          vcap_sendgrid =vcap_services["sendgrid"]
-          logger.info vcap_sendgrid
-          logger.info vcap_sendgrid.class
-          if !vcap_sendgrid.nil? 
-             sendgrid = vcap_sendgrid.first["credentials"]
-             logger.info sendgrid
-          end
-      end
+
+            logger.info '-------------------------'
+            logger.info ENV['VCAP_SERVICES'].class
+            logger.info  ENV['VCAP_SERVICES']
+            logger.info '-------------------------'
+
+            vcap_services=JSON.parse(ENV['VCAP_SERVICES'] || '{}')
+            logger.info pp vcap_services
+            if !vcap_services.empty?
+                vcap_sendgrid =vcap_services["sendgrid"]
+                logger.info vcap_sendgrid
+                logger.info vcap_sendgrid.class
+                if !vcap_sendgrid.nil? 
+                    sendgrid = vcap_sendgrid.first["credentials"]
+                    logger.info sendgrid
+                end
+            end
 
 
-      erb :index, locals: {instance: instance_number, port: port, dea: dea_host, sendgrid: sendgrid}
-    end
+            erb :index, locals: {instance: instance_number, port: port, dea: dea_host, sendgrid: sendgrid}
+        end
 
-    get "/broken" do
-      logger.debug "trying broken service"
-      `ps -AF | grep -m1 "ruby" | awk '{print $2;}' | (read pid; kill -9 $pid)`
+        get "/broken" do
+            logger.debug "trying broken service"
+            `ps -AF | grep -m1 "ruby" | awk '{print $2;}' | (read pid; kill -9 $pid)`
+        end
+
+        get "/systemcrash" do
+            logger.debug "trying broken service"
+            Kernel.exit!
+        end
+        run!
     end
-    
-    get "/systemcrash" do
-      logger.debug "trying broken service"
-      Kernel.exit!
-    end
-    run!
-  end
 
 end
 
